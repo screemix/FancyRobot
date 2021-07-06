f3d23447d898aea78adacc5c999e9f14f1da2c91
Радионов Иван Алексеевич <radionov@nkbvs.ttn.ru>
2021-07-06 10:40:08 +0300

    launch files

--- docker/into.sh
+++ docker/into.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker exec --user "docker_raai_user" -it planning_with_thunder \
-    /bin/bash -c ". /ros_entrypoint.sh; cd /home/docker_raai_user/catkin_ws; /bin/bash"
+    /bin/bash -c ". /ros_entrypoint.sh; cd /home/docker_raai_user; /bin/bash"
